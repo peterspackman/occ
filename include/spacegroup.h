1b35562e78fd2b519dce87bb5ff1c261e8965af2
Peter Spackman <peterspackman@fastmail.com>
2020-09-02 20:25:36 +0800

    Midway through updating space group to use gemmi

--- include/spacegroup.h
+++ include/spacegroup.h
@@ -1,51 +1,34 @@
 #pragma once
-#include "symmetryoperation.h"
-#include <QString>
-#include <QVector>
-#include <QMap>
-#include <QPair>
+#include <Eigen/Dense>
+#include <vector>
+#include <string>
 #include <tuple>
+#include "symmetryoperation.h"
+#include <map>
+#include <gemmi/symmetry.hpp>
+
+namespace craso::crystal {
+
+using Eigen::Matrix3Xd;
+using Eigen::VectorXi;
+
+using SGData = gemmi::SpaceGroup;
 
 class SpaceGroup
 {
-    friend class SpaceGroupTable;
 public:
-    SpaceGroup() = default;
-    SpaceGroup(const QVector<SymmetryOperation>& symops);
-    int internationalTablesNumber() const { return m_number;}
-    const QString& symbol() const { return m_symbol; }
-    void setSymbol(const QString &sym) { m_symbol = sym; }
-    const QString& choice() const { return m_choice; }
-    const QVector<SymmetryOperation>& symmetryOperations() const { return m_symops; }
-    std::pair<Eigen::VectorXi, Eigen::Matrix3Xd> applyAllSymmetryOperations(const Eigen::Matrix3Xd&) const;
-    const QString crystalSystem() const;
-    int latticeNumber() const;
-    bool hasHexagonalRhombohedralChoice() const;
+    SpaceGroup(std::string) = default;
+    SpaceGroup(const std::vector<SymmetryOperation>& symops);
+    int number() const { return m_sgdata.number;}
+    const std::string& symbol() const { return m_sgdata.hm; }
+    std::string short_name() const { return m_sgdata.short_name(); }
+    const std::vector<SymmetryOperation>& symmetry_operations() const { return m_symops; }
+    bool has_H_R_choice() const;
+    std::pair<VectorXi, Matrix3Xd> apply_all_symmetry_operations(const Eigen::Matrix3Xd&) const;
 private:
-    SpaceGroup(int, QString, QString, QString, QString, bool, QVector<SymmetryOperation>);
-    int m_number = -1;
-    QString m_symbol;
-    QString m_fullName;
-    QString m_choice;
-    QString m_centering;
-    bool m_centrosymmetric;
-    QVector<SymmetryOperation> m_symops;
+    SGData m_sgdata;
+    std::vector<SymmetryOperation> m_symops;
 };
 
-class SpaceGroupTable {
-private:
-    static SpaceGroupTable *instance;
-    SpaceGroupTable();
-    QVector<SpaceGroup> m_spaceGroups;
-    QMap<int, int> m_intToSpaceGroup;
-    QMap<QString, int> m_symbolToSpaceGroup;
-    QMap<QPair<int, QString>, int> m_intChoiceToSpaceGroup;
-public:
-    static SpaceGroupTable *table();
-    const SpaceGroup& get(int) const;
-    const SpaceGroup& get(int, const QString&) const;
-    SpaceGroup get(QVector<SymmetryOperation>) const;
-};
-
-
+}
 
