c1337158ae51424ae4e217c814577d1155923338
Peter Spackman <peterspackman@fastmail.com>
2020-10-14 10:49:09 +0800

    Almost working GGA DFT

--- src/dft.cpp
+++ src/dft.cpp
@@ -5,7 +5,6 @@
 #include <fmt/ostream.h>
 #include "logger.h"
 #include "density.h"
-
 namespace tonto::dft {
 
 template<int derivative_order>
@@ -49,14 +48,14 @@ DFT::DFT(const std::string& method, const libint2::BasisSet& basis, const std::v
     m_hf(atoms, basis), m_grid(basis, atoms)
 {
     tonto::log::debug("start calculating atom grids... ");
-    m_grid.set_max_angular_points(80);
-    m_grid.set_min_angular_points(30);
+    m_grid.set_max_angular_points(530);
+    m_grid.set_min_angular_points(80);
     for(size_t i = 0; i < atoms.size(); i++) {
         m_atom_grids.push_back(m_grid.grid_points(i));
     }
     tonto::log::debug("finished calculating atom grids");
-    m_funcs.push_back(DensityFunctional("xc_lda_x"));
-    m_funcs.push_back(DensityFunctional("xc_lda_c_vwn"));
+    m_funcs.push_back(DensityFunctional("xc_gga_x_pbe"));
+    m_funcs.push_back(DensityFunctional("xc_gga_c_pbe"));
 
     for(const auto& func: m_funcs) {
         fmt::print("Functional: {} {} {}\n", func.name(), func.kind_string(), func.family_string());
@@ -105,7 +104,7 @@ MatRM DFT::compute_2body_fock_d0(const MatRM &D, double precision, const MatRM &
         for(size_t bf = 0; bf < nbf; bf++) {
             phi_vrho.row(bf).array() *= vwt;
         }
-        K.noalias() = (gto_vals * phi_vrho.transpose());
+        K.noalias() += (gto_vals * phi_vrho.transpose());
     }
 //    fmt::print("Total density {}\n", total_density);
     m_e_alpha += D.cwiseProduct(J).sum();
@@ -115,7 +114,6 @@ MatRM DFT::compute_2body_fock_d0(const MatRM &D, double precision, const MatRM &
 
 MatRM DFT::compute_2body_fock_d1(const MatRM &D, double precision, const MatRM &Schwarz) const
 {
-    tonto::log::debug("In compute_2body_fock_d1");
     auto J = m_hf.compute_J(D, precision, Schwarz);
     tonto::MatRM K = tonto::MatRM::Zero(J.rows(), J.cols());
     const auto& basis = m_hf.basis();
@@ -148,31 +146,22 @@ MatRM DFT::compute_2body_fock_d1(const MatRM &D, double precision, const MatRM &
         for(size_t bf = 0; bf < nbf; bf++) {
             phi_vrho.row(bf).array() *= vwt;
         }
-        tonto::Mat phi_x = Eigen::Map<tonto::Mat, 0, Eigen::OuterStride<>>(gto_vals.data() + nbf, nbf, npt, {4 * gto_vals.rows()});
-        tonto::Mat phi_y = Eigen::Map<tonto::Mat, 0, Eigen::OuterStride<>>(gto_vals.data() + 2*nbf, nbf, npt, {4 * gto_vals.rows()});
-        tonto::Mat phi_z = Eigen::Map<tonto::Mat, 0, Eigen::OuterStride<>>(gto_vals.data() + 3*nbf, nbf, npt, {4 * gto_vals.rows()});
-        size_t phi_rows = phi.rows(), phi_cols = phi.cols();
+        K += phi * phi_vrho.transpose();
+        tonto::Mat phi_x = Eigen::Map<tonto::Mat, 0, Eigen::OuterStride<>>(gto_vals.data() + gto_vals.rows(), nbf, npt, {4 * gto_vals.rows()});
+        tonto::Mat phi_y = Eigen::Map<tonto::Mat, 0, Eigen::OuterStride<>>(gto_vals.data() + 2 * gto_vals.rows(), nbf, npt, {4 * gto_vals.rows()});
+        tonto::Mat phi_z = Eigen::Map<tonto::Mat, 0, Eigen::OuterStride<>>(gto_vals.data() + 3 * gto_vals.rows(), nbf, npt, {4 * gto_vals.rows()});
         for(size_t bf = 0; bf < nbf; bf++) {
             phi.row(bf).array() *= vsigmawt;
             phi_x.row(bf).array() *= rho_x;
             phi_y.row(bf).array() *= rho_y;
             phi_z.row(bf).array() *= rho_z;
         }
-        K.noalias() = (phi * phi_vrho.transpose());
-        K.noalias() += (phi * phi_x.transpose());
-        K.noalias() += (phi * phi_y.transpose());
-        K.noalias() += (phi * phi_z.transpose());
+        tonto::Mat ktmp((phi * phi_x.transpose()) + (phi * phi_y.transpose()) + (phi * phi_z.transpose()));
+        tonto::Mat KK = ktmp + ktmp.transpose();
+        K.noalias() += KK;
     }
-    fmt::print("Total density: {}\n", total_density);
-    fmt::print("exchange {}\n", m_e_alpha);
     m_e_alpha += D.cwiseProduct(J).sum();
-    fmt::print("coul {}\n", D.cwiseProduct(J).sum());
-    fmt::print("E_XC {}\n", m_e_alpha);
     tonto::Mat F = J + K;
-    fmt::print("D\n{}\n", D);
-    fmt::print("J\n{}\n", J);
-    fmt::print("K\n{}\n", K);
-    fmt::print("F\n{}\n", F);
     return F;
 }
 
