e8e421198ab0f80d39f949d007aa0a164beac813
Peter Spackman <peterspackman@fastmail.com>
2020-10-15 19:49:04 +0800

    Removed old slow, wrong gto code

--- include/density.h
+++ include/density.h
@@ -4,23 +4,14 @@
 
 namespace tonto::density {
 
-inline int num_components(int deriv_order) {
-    switch(deriv_order) {
-    case 0: return 1;
-    case 1: return 4;
-    case 2: return 10;
+    constexpr int num_components(int deriv_order) {
+        switch(deriv_order) {
+        case 0: return 1;
+        case 1: return 4;
+        case 2: return 10;
+        }
+        return 1;
     }
-    return 1;
-}
-
-    void eval_shell(const libint2::Shell &s1, const Eigen::Ref<const tonto::Mat>& dists, Eigen::Ref<tonto::Mat>& result, int derivative=0);
-    tonto::MatRM evaluate_gtos(
-            const libint2::BasisSet &basis, const std::vector<libint2::Atom> &atoms,
-            const tonto::MatN4 &grid_pts, int derivative=0);
-    tonto::Mat evaluate(
-            const libint2::BasisSet &basis, const std::vector<libint2::Atom> &atoms,
-            const tonto::MatRM& D, const tonto::MatN4 &grid_pts, int derivative=0);
-
 
     template<size_t max_derivative>
     tonto::Mat evaluate_density(const tonto::MatRM &D, const tonto::gto::GTOValues<max_derivative>& gto_values)
