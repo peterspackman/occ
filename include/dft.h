1f350b21264645320304ec2549e8bdad6d9c7bd3
Peter Spackman <peterspackman@fastmail.com>
2020-10-21 12:41:24 +0800

    Parallelized DFT evaluation

--- include/dft.h
+++ include/dft.h
@@ -10,6 +10,7 @@
 #include "hf.h"
 #include "gto.h"
 #include "density.h"
+#include "parallel.h"
 
 namespace libint2 {
 class BasisSet;
@@ -108,6 +109,7 @@ public:
     template<int derivative_order, SpinorbitalKind spinorbital_kind = SpinorbitalKind::Restricted>
     MatRM compute_fock_dft(const MatRM &D, double precision, const MatRM& Schwarz)
     {
+        using tonto::parallel::nthreads;
         tonto::MatRM K, F;
         m_e_alpha = 0.0;
         double ecoul, exc;
@@ -127,127 +129,129 @@ public:
         K = tonto::MatRM::Zero(F.rows(), F.cols());
         const auto& basis = m_hf.basis();
         const auto& atoms = m_hf.atoms();
-        constexpr size_t BLOCKSIZE = 256;
+        constexpr size_t BLOCKSIZE = 128;
         double total_density_a{0.0}, total_density_b{0.0};
-        auto D2 = 2 * D;
+        const auto& D2 = 2 * D;
         DensityFunctional::Family family{DensityFunctional::Family::LDA};
         if constexpr (derivative_order == 1) {
             family = DensityFunctional::Family::GGA;
         }
 
+        std::vector<tonto::MatRM> Kt(tonto::parallel::nthreads, tonto::Mat::Zero(D.rows(), D.cols()));
+        std::vector<double> energies(tonto::parallel::nthreads, 0.0);
+        const auto& funcs = m_funcs;
         for(const auto& [atom_pts, atom_weights] : m_atom_grids) {
-            size_t npt_total = atom_pts.cols();
-            size_t num_blocks = npt_total / BLOCKSIZE + 1;
+            const size_t npt_total = atom_pts.cols();
+            const size_t num_blocks = npt_total / BLOCKSIZE + 1;
 
-            for(size_t block = 0; block < num_blocks; block++) {
-                Eigen::Index l = block * BLOCKSIZE;
-                Eigen::Index u = std::min(npt_total - 1, (block + 1) * BLOCKSIZE);
-                Eigen::Index npt = u - l;
-                if(npt == 0) continue;
-                const auto& pts_block = atom_pts.middleCols(l, npt);
-                const auto& weights_block = atom_weights.segment(l, npt);
-                DensityFunctional::Params params(npt, family, spinorbital_kind);
-                tonto::timing::start(tonto::timing::category::grid);
-                const auto [rho, gto_vals] = evaluate_density_and_gtos<derivative_order, spinorbital_kind>(basis, atoms, D2, pts_block);
-                tonto::timing::stop(tonto::timing::category::grid);
+            auto lambda = [&](int thread_id)
+            {
+                for(size_t block = 0; block < num_blocks; block++) {
+                    if(block % nthreads != thread_id) continue;
+                    Eigen::Index l = block * BLOCKSIZE;
+                    Eigen::Index u = std::min(npt_total - 1, (block + 1) * BLOCKSIZE);
+                    Eigen::Index npt = u - l;
+                    if(npt == 0) continue;
+                    auto& k = Kt[thread_id];
+                    const auto& pts_block = atom_pts.middleCols(l, npt);
+                    const auto& weights_block = atom_weights.segment(l, npt);
+                    DensityFunctional::Params params(npt, family, spinorbital_kind);
+                    const auto [rho, gto_vals] = evaluate_density_and_gtos<derivative_order, spinorbital_kind>(basis, atoms, D2, pts_block);
 
-                if constexpr (spinorbital_kind == SpinorbitalKind::Restricted) {
-                    params.rho.col(0) = rho.col(0);
-                    total_density_a += rho.col(0).dot(weights_block);
-                }
-                else if constexpr (spinorbital_kind == SpinorbitalKind::Unrestricted) {
-                    // correct assignment
-                    params.rho.col(0) = rho.alpha().col(0);
-                    params.rho.col(1) = rho.beta().col(0);
-                    total_density_a += rho.alpha().col(0).dot(weights_block);
-                    total_density_b += rho.beta().col(0).dot(weights_block);
-                }
-
-
-                if constexpr(derivative_order > 0) {
-                    if constexpr(spinorbital_kind == SpinorbitalKind::Restricted) {
-                        const auto& dx_rho = rho.col(1).array(), dy_rho = rho.col(2).array(), dz_rho = rho.col(3).array();
-                        params.sigma.col(0) = dx_rho * dx_rho + dy_rho * dy_rho + dz_rho * dz_rho;
-                    }
-                    else if constexpr(spinorbital_kind == SpinorbitalKind::Unrestricted) {
-
-                        const auto& rho_alpha = rho.alpha();
-                        const auto& rho_beta = rho.beta();
-                        const auto& dx_rho_a = rho_alpha.col(1).array();
-                        const auto& dy_rho_a = rho_alpha.col(2).array();
-                        const auto& dz_rho_a = rho_alpha.col(3).array();
-                        const auto& dx_rho_b = rho_beta.col(1).array();
-                        const auto& dy_rho_b = rho_beta.col(2).array();
-                        const auto& dz_rho_b = rho_beta.col(3).array();
-                        params.sigma.col(0) = dx_rho_a * dx_rho_a + dy_rho_a * dy_rho_a + dz_rho_a * dz_rho_a;
-                        params.sigma.col(1) = dx_rho_a * dx_rho_b + dy_rho_a * dy_rho_b + dz_rho_a * dz_rho_b;
-                        params.sigma.col(2) = dx_rho_b * dx_rho_b + dy_rho_b * dy_rho_b + dz_rho_b * dz_rho_b;
-                    }
-                }
-
-                DensityFunctional::Result res(npt, family, spinorbital_kind);
-                tonto::timing::start(tonto::timing::category::dft);
-                for(const auto& func: m_funcs) {
-                    res += func.evaluate(params);
-                }
-                tonto::timing::stop(tonto::timing::category::dft);
-
-                tonto::MatRM KK = tonto::MatRM::Zero(K.rows(), K.cols());
-
-                // Weight the arrays by the grid weights
-                res.weight_by(weights_block);
-                if constexpr(spinorbital_kind == SpinorbitalKind::Restricted) {
-                    m_e_alpha += rho.col(0).dot(res.exc);
-                    tonto::Mat phi_vrho = gto_vals.phi.array().colwise() * res.vrho.col(0).array();
-                    KK = gto_vals.phi.transpose() * phi_vrho;
-                } else if constexpr(spinorbital_kind == SpinorbitalKind::Unrestricted) {
-                    m_e_alpha += res.exc.dot(rho.alpha().col(0)) + res.exc.dot(rho.beta().col(0));
-                    tonto::Mat phi_vrho_a = gto_vals.phi.array().colwise() * res.vrho.col(0).array();
-                    tonto::Mat phi_vrho_b = gto_vals.phi.array().colwise() * res.vrho.col(1).array();
-
-                    KK.alpha().noalias() = gto_vals.phi.transpose() * phi_vrho_a;
-                    KK.beta().noalias() = gto_vals.phi.transpose() * phi_vrho_b;
-                }
-
-                if constexpr(derivative_order > 0) {
                     if constexpr (spinorbital_kind == SpinorbitalKind::Restricted) {
-                        auto g = rho.block(0, 1, npt, 3).array().colwise() * (2 * res.vsigma.col(0).array());
-                        tonto::Mat gamma = gto_vals.phi_x.array().colwise() * g.col(0).array()
-                                         + gto_vals.phi_y.array().colwise() * g.col(1).array()
-                                         + gto_vals.phi_z.array().colwise() * g.col(2).array();
-                        tonto::Mat ktmp = gto_vals.phi.transpose() * gamma;
-                        KK.noalias() += ktmp + ktmp.transpose();
-
-                    } else if constexpr (spinorbital_kind == SpinorbitalKind::Unrestricted) {
-                        auto ga = rho.alpha().block(0, 1, npt, 3).array().colwise() * (2 * res.vsigma.col(0).array()) +
-                                        rho.beta().block(0, 1, npt, 3).array().colwise() * res.vsigma.col(1).array();
-                        auto gb = rho.beta().block(0, 1, npt, 3).array().colwise() * (2 * res.vsigma.col(2).array()) +
-                                        rho.alpha().block(0, 1, npt, 3).array().colwise() * res.vsigma.col(1).array();
-
-                        tonto::Mat gamma_a = gto_vals.phi_x.array().colwise() * ga.col(0).array()
-                                           + gto_vals.phi_y.array().colwise() * ga.col(1).array()
-                                           + gto_vals.phi_z.array().colwise() * ga.col(2).array();
-                        tonto::Mat gamma_b = gto_vals.phi_x.array().colwise() * gb.col(0).array()
-                                           + gto_vals.phi_y.array().colwise() * gb.col(1).array()
-                                           + gto_vals.phi_z.array().colwise() * gb.col(2).array();
-                        tonto::Mat ktmp_a = (gto_vals.phi.transpose() * gamma_a);
-                        tonto::Mat ktmp_b = (gto_vals.phi.transpose() * gamma_b);
-                        KK.alpha().noalias() += ktmp_a + ktmp_a.transpose();
-                        KK.beta().noalias() += ktmp_b + ktmp_b.transpose();
+                        params.rho.col(0) = rho.col(0);
+                        total_density_a += rho.col(0).dot(weights_block);
                     }
+                    else if constexpr (spinorbital_kind == SpinorbitalKind::Unrestricted) {
+                        // correct assignment
+                        params.rho.col(0) = rho.alpha().col(0);
+                        params.rho.col(1) = rho.beta().col(0);
+                        total_density_a += rho.alpha().col(0).dot(weights_block);
+                        total_density_b += rho.beta().col(0).dot(weights_block);
+                    }
+
+
+                    if constexpr(derivative_order > 0) {
+                        if constexpr(spinorbital_kind == SpinorbitalKind::Restricted) {
+                            const auto& dx_rho = rho.col(1).array(), dy_rho = rho.col(2).array(), dz_rho = rho.col(3).array();
+                            params.sigma.col(0) = dx_rho * dx_rho + dy_rho * dy_rho + dz_rho * dz_rho;
+                        }
+                        else if constexpr(spinorbital_kind == SpinorbitalKind::Unrestricted) {
+
+                            const auto& rho_alpha = rho.alpha();
+                            const auto& rho_beta = rho.beta();
+                            const auto& dx_rho_a = rho_alpha.col(1).array();
+                            const auto& dy_rho_a = rho_alpha.col(2).array();
+                            const auto& dz_rho_a = rho_alpha.col(3).array();
+                            const auto& dx_rho_b = rho_beta.col(1).array();
+                            const auto& dy_rho_b = rho_beta.col(2).array();
+                            const auto& dz_rho_b = rho_beta.col(3).array();
+                            params.sigma.col(0) = dx_rho_a * dx_rho_a + dy_rho_a * dy_rho_a + dz_rho_a * dz_rho_a;
+                            params.sigma.col(1) = dx_rho_a * dx_rho_b + dy_rho_a * dy_rho_b + dz_rho_a * dz_rho_b;
+                            params.sigma.col(2) = dx_rho_b * dx_rho_b + dy_rho_b * dy_rho_b + dz_rho_b * dz_rho_b;
+                        }
+                    }
+
+                    DensityFunctional::Result res(npt, family, spinorbital_kind);
+                    for(const auto& func: funcs) {
+                        res += func.evaluate(params);
+                    }
+
+                    tonto::MatRM KK = tonto::MatRM::Zero(k.rows(), k.cols());
+
+                    // Weight the arrays by the grid weights
+                    res.weight_by(weights_block);
+                    if constexpr(spinorbital_kind == SpinorbitalKind::Restricted) {
+                        energies[thread_id] += rho.col(0).dot(res.exc);
+                        tonto::Mat phi_vrho = gto_vals.phi.array().colwise() * res.vrho.col(0).array();
+                        KK = gto_vals.phi.transpose() * phi_vrho;
+                    } else if constexpr(spinorbital_kind == SpinorbitalKind::Unrestricted) {
+                        energies[thread_id] += res.exc.dot(rho.alpha().col(0)) + res.exc.dot(rho.beta().col(0));
+                        tonto::Mat phi_vrho_a = gto_vals.phi.array().colwise() * res.vrho.col(0).array();
+                        tonto::Mat phi_vrho_b = gto_vals.phi.array().colwise() * res.vrho.col(1).array();
+
+                        KK.alpha().noalias() = gto_vals.phi.transpose() * phi_vrho_a;
+                        KK.beta().noalias() = gto_vals.phi.transpose() * phi_vrho_b;
+                    }
+
+                    if constexpr(derivative_order > 0) {
+                        if constexpr (spinorbital_kind == SpinorbitalKind::Restricted) {
+                            auto g = rho.block(0, 1, npt, 3).array().colwise() * (2 * res.vsigma.col(0).array());
+                            tonto::Mat gamma = gto_vals.phi_x.array().colwise() * g.col(0).array()
+                                             + gto_vals.phi_y.array().colwise() * g.col(1).array()
+                                             + gto_vals.phi_z.array().colwise() * g.col(2).array();
+                            tonto::Mat ktmp = gto_vals.phi.transpose() * gamma;
+                            KK.noalias() += ktmp + ktmp.transpose();
+
+                        } else if constexpr (spinorbital_kind == SpinorbitalKind::Unrestricted) {
+                            auto ga = rho.alpha().block(0, 1, npt, 3).array().colwise() * (2 * res.vsigma.col(0).array()) +
+                                            rho.beta().block(0, 1, npt, 3).array().colwise() * res.vsigma.col(1).array();
+                            auto gb = rho.beta().block(0, 1, npt, 3).array().colwise() * (2 * res.vsigma.col(2).array()) +
+                                            rho.alpha().block(0, 1, npt, 3).array().colwise() * res.vsigma.col(1).array();
+
+                            tonto::Mat gamma_a = gto_vals.phi_x.array().colwise() * ga.col(0).array()
+                                               + gto_vals.phi_y.array().colwise() * ga.col(1).array()
+                                               + gto_vals.phi_z.array().colwise() * ga.col(2).array();
+                            tonto::Mat gamma_b = gto_vals.phi_x.array().colwise() * gb.col(0).array()
+                                               + gto_vals.phi_y.array().colwise() * gb.col(1).array()
+                                               + gto_vals.phi_z.array().colwise() * gb.col(2).array();
+                            tonto::Mat ktmp_a = (gto_vals.phi.transpose() * gamma_a);
+                            tonto::Mat ktmp_b = (gto_vals.phi.transpose() * gamma_b);
+                            KK.alpha().noalias() += ktmp_a + ktmp_a.transpose();
+                            KK.beta().noalias() += ktmp_b + ktmp_b.transpose();
+                        }
+                    }
+                    k.noalias() += KK;
                 }
-                K.noalias() += KK;
-            }
+            };
+            tonto::parallel::parallel_do(lambda);
             //tonto::log::debug("E_coul: {}, E_x: {}, E_xc = {}, E_XC = {}", ecoul, exc, m_e_alpha, m_e_alpha + exc);
-
-
         }
-        fmt::print("Total density: alpha = {} beta = {}\nGTO  {:10.5f}\nfunc {:10.5f}\nfock {:10.5f}\n\n",
-                   total_density_a, total_density_b,
-                        tonto::timing::total(tonto::timing::category::grid),
-                        tonto::timing::total(tonto::timing::category::dft),
-                        tonto::timing::total(tonto::timing::category::ints));
-        tonto::timing::clear_all();
+
+        for(size_t i = 0; i < nthreads; i++) {
+            K += Kt[i];
+            m_e_alpha += energies[i];
+        }
         m_e_alpha += exc + ecoul;
         F += K;
         return F;
