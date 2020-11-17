1a292378b86d8af35ef0051cb84b2eb822d7b4d9
Peter Spackman <peterspackman@fastmail.com>
2020-11-17 22:42:37 +0800

    tidy up parallel formatting

--- include/parallel.h
+++ include/parallel.h
@@ -6,11 +6,11 @@ inline int nthreads = 1;
 
 /// fires off \c nthreads instances of lambda in parallel
 template <typename Lambda> void parallel_do(Lambda &lambda) {
-#pragma omp parallel
-  {
-    auto thread_id = omp_get_thread_num();
-    lambda(thread_id);
-  }
+    #pragma omp parallel
+    {
+        auto thread_id = omp_get_thread_num();
+        lambda(thread_id);
+    }
 }
 
 } // namespace tonto::parallel
