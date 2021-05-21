b953528a3de0b2d063399531bf7d96c7f14db653
Peter Spackman <peterspackman@fastmail.com>
2021-05-21 01:44:35 +0000

    Added shell_order header

---
+++ include/occ/gto/shell_order.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <libint2/shgshell_ordering.h>
+#include <libint2/cgshell_ordering.h>
+
+namespace occ::gto {
+
+template<bool cartesian, typename F>
+inline void iterate_over_shell(F &f, int l)
+{
+    if constexpr(cartesian)
+    {
+        int i, j, k;
+        FOR_CART(i, j, k, l)
+            f(i, j, k, l);
+        END_FOR_CART
+    }
+    else
+    {
+        int m;
+        FOR_SOLIDHARM(l, m)
+            f(l, m);
+        END_FOR_SOLIDHARM
+    }
+}
+
+}
