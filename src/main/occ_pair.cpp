e39bbe1749c173807f283eff3b723e827a1c22f7
Peter Spackman <peterspackman@fastmail.com>
2021-12-09 13:13:46 +0800

    Update occ-pair to use json input file

--- src/main/occ_pair.cpp
+++ src/main/occ_pair.cpp
@@ -12,77 +12,24 @@
 #include <occ/io/moldenreader.h>
 #include <occ/qm/hf.h>
 #include <occ/qm/wavefunction.h>
-#include <toml.hpp>
+#include <nlohmann/json.hpp>
 
 using occ::interaction::CEModelInteraction;
 using occ::qm::Wavefunction;
+using occ::Mat3;
+using occ::Vec3;
 
 constexpr double kjmol_per_hartree{2625.46};
 
-namespace impl {
-struct rotation {
-    Eigen::Matrix3d mat{Eigen::Matrix3d::Identity(3, 3)};
+struct Pair {
+    struct Monomer {
+        Wavefunction wfn;
+        occ::Mat3 rotation{occ::Mat3::Identity()};
+        occ::Vec3 translation{occ::Vec3::Zero()};
+    };
+    Monomer a;
+    Monomer b;
 };
-
-struct translation {
-    Eigen::Vector3d vec{Eigen::Vector3d::Zero(3)};
-};
-
-} // namespace impl
-
-namespace toml {
-template <> struct from<impl::rotation> {
-    static impl::rotation from_toml(const value &v) {
-        impl::rotation rot;
-        auto arr = toml::get<toml::array>(v);
-        if (arr.size() == 9) {
-            for (size_t i = 0; i < 9; i++) {
-                double v = arr[i].is_floating()
-                               ? arr[i].as_floating(std::nothrow)
-                               : static_cast<double>(arr[i].as_integer());
-                rot.mat(i / 3, i % 3) = v;
-            }
-        } else if (arr.size() == 3) {
-            for (size_t i = 0; i < 3; i++) {
-                auto row = toml::get<toml::array>(arr[i]);
-                for (size_t j = 0; j < 3; j++) {
-                    double v = row[j].is_floating()
-                                   ? row[j].as_floating(std::nothrow)
-                                   : static_cast<double>(row[j].as_integer());
-                    rot.mat(i, j) = v;
-                }
-            }
-        } else {
-            std::cerr << toml::format_error(
-                             "[error] 3D rotation matrix has invalid length", v,
-                             "Expecting a [3, 3] or [9] array of int or double")
-                      << std::endl;
-        }
-        return rot;
-    }
-};
-
-template <> struct from<impl::translation> {
-    static impl::translation from_toml(const value &v) {
-        impl::translation trans;
-        auto arr = toml::get<toml::array>(v);
-        if (arr.size() == 3) {
-            for (size_t i = 0; i < 3; i++) {
-                double v = arr[i].is_floating()
-                               ? arr[i].as_floating(std::nothrow)
-                               : static_cast<double>(arr[i].as_integer());
-                trans.vec(i) = v;
-            }
-        } else {
-            std::cerr << toml::format_error(
-                             "[error] 3D translation vector has invalid length",
-                             v, "Expecting a [3] array of int or double")
-                      << std::endl;
-        }
-        return trans;
-    }
-};
-} // namespace toml
 
 Wavefunction load_wavefunction(const std::string &filename) {
     namespace fs = std::filesystem;
@@ -103,47 +50,77 @@ Wavefunction load_wavefunction(const std::string &filename) {
         "Unknown file extension when reading wavefunction: " + ext);
 }
 
-int main(int argc, const char **argv) {
-    const auto input = toml::parse((argc > 1) ? argv[1] : "ce.toml");
-    const auto pair_interaction_table = toml::find(input, "interaction");
-    const auto global_settings_table = toml::find(input, "global");
+void load_matrix(const nlohmann::json &json, Mat3 &mat) {
+    for(size_t i = 0; i < json.size(); i++) {
+        const auto& row = json.at(i);
+        for (size_t j = 0; j < row.size(); j++) {
+            mat(i, j) = row.at(j).get<double>();
+        }
+    }
+}
+
+void load_vector(const nlohmann::json &json, Vec3 &vec) {
+    for(size_t i = 0; i < json.size(); i++) {
+        vec(i) = json.at(i).get<double>();
+    }
+}
+
+Pair parse_input_file(const std::string &filename) {
+    std::ifstream i(filename);
+    nlohmann::json j;
+    i >> j;
+    const auto& monomers = j["monomers"];
+
+    if(monomers.size() != 2) throw std::runtime_error("Require two monomers in input file");
+
+    Pair p;
+    std::string source_a = monomers[0]["source"];
+    std::string source_b = monomers[1]["source"];
+    p.a.wfn = load_wavefunction(source_a);
+    load_matrix(monomers[0]["rotation"], p.a.rotation);
+    load_vector(monomers[0]["translation"], p.a.translation);
+
+    p.b.wfn = load_wavefunction(source_b);
+    load_matrix(monomers[1]["rotation"], p.b.rotation);
+    load_vector(monomers[1]["translation"], p.b.translation);
+
+    p.a.wfn.apply_transformation(p.a.rotation, p.a.translation);
+    p.b.wfn.apply_transformation(p.b.rotation, p.b.translation);
+    return p;
+}
+
+int main(int argc, char *argv[]) {
+    cxxopts::Options options("occ", "A program for quantum chemistry");
+    options.positional_help("[input_file]")
+        .show_positional_help();
+
+    options.add_options()("h,help", "Print help")(
+        "i,input", "Input file", cxxopts::value<std::string>())(
+        "t,threads", "Number of threads",
+        cxxopts::value<int>()->default_value("1"))(
+        "m,model", "CE model",
+        cxxopts::value<std::string>()->default_value("ce-b3lyp"))(
+        "v,verbosity", "Logging verbosity",
+        cxxopts::value<std::string>()->default_value("WARN"));
+
+    options.parse_positional({"input"});
+    auto args = options.parse(argc, argv);
+
     occ::timing::start(occ::timing::category::global);
     libint2::Shell::do_enforce_unit_normalization(true);
     libint2::initialize();
 
+    auto pair = parse_input_file(args["input"].as<std::string>());
+
     using occ::parallel::nthreads;
-    nthreads = toml::find_or<int>(global_settings_table, "threads", 1);
+    nthreads = args["threads"].as<int>();
 
-    const std::string model_name =
-        toml::find_or<std::string>(pair_interaction_table, "model", "ce-b3lyp");
-    const std::string filename_a = toml::find_or<std::string>(
-        pair_interaction_table, "monomer_a", "a.fchk");
-    const std::string filename_b = toml::find_or<std::string>(
-        pair_interaction_table, "monomer_b", "b.fchk");
-    occ::Mat3 rotation_a =
-        toml::find_or<impl::rotation>(pair_interaction_table, "rotation_a",
-                                      impl::rotation{})
-            .mat;
-    occ::Mat3 rotation_b =
-        toml::find_or<impl::rotation>(pair_interaction_table, "rotation_b",
-                                      impl::rotation{})
-            .mat;
-    occ::Vec3 translation_a =
-        toml::find_or<impl::translation>(pair_interaction_table,
-                                         "translation_a", impl::translation{})
-            .vec;
-    occ::Vec3 translation_b =
-        toml::find_or<impl::translation>(pair_interaction_table,
-                                         "translation_b", impl::translation{})
-            .vec;
+    const std::string model_name = args["model"].as<std::string>();
 
-    Wavefunction A = load_wavefunction(filename_a);
-    A.apply_transformation(rotation_a, translation_a);
-    Wavefunction B = load_wavefunction(filename_b);
-    B.apply_transformation(rotation_b, translation_b);
     auto model = occ::interaction::ce_model_from_string(model_name);
+
     CEModelInteraction interaction(model);
-    auto interaction_energy = interaction(A, B);
+    auto interaction_energy = interaction(pair.a.wfn, pair.b.wfn);
     occ::timing::stop(occ::timing::category::global);
 
     fmt::print("Component              Energy (kJ/mol)\n\n");
