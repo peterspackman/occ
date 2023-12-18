b199b7ca7300b74672fe420b9d65589e833a9721
Peter Spackman <peterspackman@fastmail.com>
2023-12-18 13:05:38 +0800

    Fixup initial guess again, stop special casing elements

--- src/qm/guess_density.cpp
+++ src/qm/guess_density.cpp
@@ -30,19 +30,17 @@ int minimal_basis_nao(int Z, bool spherical) {
     if (Z == 1 || Z == 2) // H, He
         nao = 1;
     else if (Z <= 10)              // Li - Ne
-        nao = 5;                   // 2p is included even for Li and Be
+        nao = 5;
     else if (Z <= 18)              // Na - Ar
-        nao = 9;                   // 3p is included even for Na and Mg
+        nao = 9;
     else if (Z < 20)               // K, Ca
-        nao = 13;                  // 4p is included
+        nao = 13;
     else if (Z <= 36)              // Sc - Kr
-        nao = spherical ? 18 : 19; // 1 D function = 1 extra
-    else if (Z <= 38)              // Rb, Sr
-        nao = spherical ? 22 : 23; // 5p is included, 1 D function = 1 extra
-    else if (Z <= 53)              // Y - I
-        nao = spherical ? 27 : 29; // 2 D functions = 2 extra
+        nao = spherical ? 18 : 19;
+    else if (Z <= 54)              // Rb - Xe
+        nao = spherical ? 27 : 29;
     else if (Z <= 86)
-        nao = spherical ? 49 : 55; // 3 D functions, 1 F = 6 extra functions
+        nao = spherical ? 40 : 46; // 3 D functions, 1 F = 6 extra functions
     else
         throw "minimal basis not defined for elements Z > 86";
     return nao;
@@ -57,82 +55,60 @@ std::vector<double> minimal_basis_occupation_vector(size_t Z, bool spherical) {
 
     int num_of_electrons = Z;
     int dsize = spherical ? 5 : 6;
-    int fsize = spherical ? 9 : 10;
+    int fsize = spherical ? 7 : 10;
 
-    update_occupation_subshell(occvec, 1, num_of_electrons);     // 1s
-    if (Z > 2) {                                                 // Li+
+    // Fill 1s
+    update_occupation_subshell(occvec, 1, num_of_electrons);
+
+    // Fill 2s, 2p
+    if (Z > 2) {
         update_occupation_subshell(occvec, 1, num_of_electrons); // 2s
         update_occupation_subshell(occvec, 3, num_of_electrons); // 2p
     }
-    if (Z > 10) {                                                // Na+
+
+    // Fill 3s, 3p
+    if (Z > 10) {
         update_occupation_subshell(occvec, 1, num_of_electrons); // 3s
         update_occupation_subshell(occvec, 3, num_of_electrons); // 3p
     }
-    if (18 < Z && Z <= 36) { // K .. Kr
-        // 4s is singly occupied in K, Cr, and Cu
-        int num_of_4s_electrons = (Z == 19 || Z == 24 || Z == 29) ? 1 : 2;
-        num_of_electrons -= num_of_4s_electrons;
-        update_occupation_subshell(occvec, 1, num_of_4s_electrons); // 4s
 
-        int num_of_4p_electrons =
-            std::min(6, (Z > 30) ? static_cast<int>(Z) - 30 : 0);
-        num_of_electrons -= num_of_4p_electrons;
-        update_occupation_subshell(occvec, 3, num_of_4p_electrons); // 4p
-
-        update_occupation_subshell(occvec, dsize,
-                                   num_of_electrons); // 3d
-    }
-    if (36 < Z && Z <= 53) { // Rb .. I
-        // 3d4s4p are fully occupied ...
+    // Fill 4s, 3d, 4p
+    if (Z > 18) {
         update_occupation_subshell(occvec, 1, num_of_electrons); // 4s
+        update_occupation_subshell(occvec, dsize, num_of_electrons); // 3d
         update_occupation_subshell(occvec, 3, num_of_electrons); // 4p
-
-        // 5s is singly occupied in Rb, Nb, Mo, Ru, Rh, and Ag
-        int num_of_5s_electrons{2};
-        switch (Z) {
-        case 37:
-        case 41:
-        case 42:
-        case 44:
-        case 45:
-        case 47:
-            num_of_5s_electrons = 1;
-            break;
-        default:
-            break;
-        }
-        num_of_electrons -= num_of_5s_electrons;
-        update_occupation_subshell(occvec, 1, num_of_5s_electrons); // 5s
-
-        int num_of_5p_electrons =
-            std::min(static_cast<decltype(Z)>(6), (Z > 48) ? Z - 48 : 0);
-        num_of_electrons -= num_of_5p_electrons;
-        update_occupation_subshell(occvec, 3, num_of_5p_electrons); // 5p
-
-        update_occupation_subshell(occvec, dsize, num_of_electrons); // 3d
-        update_occupation_subshell(occvec, dsize, num_of_electrons); // 4d
     }
-    if (53 < Z && Z <= 86) {
-        // TODO proper setup for heavy elements!
-        update_occupation_subshell(occvec, 1, num_of_electrons);     // 4s
-        update_occupation_subshell(occvec, 3, num_of_electrons);     // 4p
-        update_occupation_subshell(occvec, 1, num_of_electrons);     // 5s
-        update_occupation_subshell(occvec, 3, num_of_electrons);     // 5p
-        update_occupation_subshell(occvec, 3, num_of_electrons);     // 6p
-        update_occupation_subshell(occvec, 3, num_of_electrons);     // 6p
-        update_occupation_subshell(occvec, dsize, num_of_electrons); // 3d
+
+    // Fill 5s, 4d, 5p
+    if (Z > 36) {
+        update_occupation_subshell(occvec, 1, num_of_electrons); // 5s
         update_occupation_subshell(occvec, dsize, num_of_electrons); // 4d
-        update_occupation_subshell(occvec, dsize, num_of_electrons); // 5d
+        update_occupation_subshell(occvec, 3, num_of_electrons); // 5p
+    }
+
+    // Fill 6s, 4f, 5d, 6p
+    if (Z > 54) {
+        update_occupation_subshell(occvec, 1, num_of_electrons); // 6s
         update_occupation_subshell(occvec, fsize, num_of_electrons); // 4f
+        update_occupation_subshell(occvec, dsize, num_of_electrons); // 5d
+        update_occupation_subshell(occvec, 3, num_of_electrons); // 6p
     }
 
+    // Fill 7s, 5f, 6d, 7p
+    if (Z > 86) {
+        update_occupation_subshell(occvec, 1, num_of_electrons); // 7s
+        update_occupation_subshell(occvec, fsize, num_of_electrons); // 5f
+        update_occupation_subshell(occvec, dsize, num_of_electrons); // 6d
+        update_occupation_subshell(occvec, 3, num_of_electrons); // 7p
+    }
+
+    // Check for any errors in occupation vector size
     if (occvec.size() != nao) {
         occ::log::warn(
             "Inconsistent number of atomic orbitals in minimal basis "
             "occupation vector: expected {}, have {}",
             nao, occvec.size());
     }
-
     return occvec;
 }
 
