05b9e8dc2ae1dcb3707933a508ab4dc77a51c8ce
Peter Spackman <peterspackman@fastmail.com>
2024-02-26 18:32:12 +0800

    Rework build to parallelise wheels

--- src/occpy.cpp
+++ src/occpy.cpp
@@ -409,6 +409,6 @@ NB_MODULE(_occpy, m) {
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
 #else
-    m.attr("__version__") = "0.5.5";
+    m.attr("__version__") = "0.5.6";
 #endif
 }
