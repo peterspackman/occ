aececdd5262a2646be7ced20553c3d61a019e5f8
Peter Spackman <peterspackman@fastmail.com>
2025-08-06 16:11:15 +0800

    Bump version number to 0.7.10

--- src/occpy.cpp
+++ src/occpy.cpp
@@ -51,6 +51,6 @@ NB_MODULE(_occpy, m) {
 #ifdef VERSION_INFO
   m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
 #else
-  m.attr("__version__") = "0.7.9";
+  m.attr("__version__") = "0.7.10";
 #endif
 }
