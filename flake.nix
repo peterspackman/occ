4b78e3d44140f0787c54c6388a94b6355d1ea321
LucasMilitao <lucas.milito@usp.br>
2026-02-23 19:11:38 -0300

    temp

--- flake.nix
+++ flake.nix
@@ -59,34 +59,37 @@
         version = "0.6.5";
         src = gemmi-src;
       };
+      python = pkgs.python314.withPackages (ps: [ ps.nanobind ]);
+      CMAKE_PREFIX_PATH = "${python}/${python.sitePackages}";
     in
     {
       # 1. The Build Artifact (nix build)
       packages.${system} = {
+        python = python;
         default = pkgs.stdenv.mkDerivation {
           name = "occ";
           src = ./.;
           postPatch = ''
             rm cmake/FindLibxc.cmake
           '';
-          buildInputs = [
-            pkgs.cmake
-            # Project dependencies installed with cpm.cmake
-            pkgs.cpm-cmake
-            pkgs.pkg-config
-            pkgs.spdlog
-            pkgs.onetbb
-            pkgs.tomlplusplus
-            pkgs.unordered_dense
-            pkgs.fmt
-            pkgs.nlohmann_json
-            pkgs.eigen_3_4_0
-            pkgs.libxc.dev
-            pkgs.cli11
+          buildInputs = with pkgs; [
+            cmake
+            python
+            cpm-cmake
+            pkg-config
+            spdlog
+            onetbb
+            tomlplusplus
+            unordered_dense
+            fmt
+            nlohmann_json
+            eigen_3_4_0
+            libxc.dev
+            cli11
             gemmi
-            pkgs.ccache
-            pkgs.gfortran.cc.lib
-            pkgs.zlib
+            ccache
+            gfortran.cc.lib
+            zlib
             scnlib
             dftd4
             libcint
@@ -103,6 +106,7 @@
             CCACHE_DIR = "/var/cache/ccache";
             CCACHE_BASEDIR = "$NIX_BUILD_TOP";
             CCACHE_SLOPPINESS = "locale,time_macros";
+            CMAKE_PREFIX_PATH = "${CMAKE_PREFIX_PATH}";
           };
           cmakeFlags = [
             "-GNinja"
@@ -113,6 +117,7 @@
             "-DCMAKE_C_COMPILER_LAUNCHER=${pkgs.ccache}/bin/ccache"
             "-DCMAKE_CXX_COMPILER_LAUNCHER=${pkgs.ccache}/bin/ccache"
             "-DNIX_BUILD=ON"
+            "-DWITH_PYTHON_BINDINGS=ON"
           ];
           NIX_LDFLAGS = "-lquadmath";
         };
@@ -122,17 +127,59 @@
 
       # 2. The Development Environment (nix develop)
       devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          cmake
-          cargo
+        inputsFrom = [ self.packages.${system}.default ];
+
+        # Extra development-only tools
+        nativeBuildInputs = with pkgs; [
+          llvm
           gcc
-          clang-tools
-          spdlog
-          lbfgspp
           gdb
-          libcint
-          pkg-config
+          ninja
+          clang-tools
+          lld
         ];
+
+        shellHook =
+          let
+            # List all dependencies that CMake needs to find
+            deps = [
+              pkgs.spdlog
+              pkgs.onetbb
+              pkgs.tomlplusplus
+              pkgs.unordered_dense
+              pkgs.fmt
+              pkgs.nlohmann_json
+              pkgs.eigen_3_4_0
+              pkgs.libxc
+              pkgs.cli11
+              gemmi
+              scnlib
+              dftd4
+              libcint
+              lbfgspp
+              fast-float-6_1_6
+            ];
+            runtimeLibs = with pkgs; [
+              stdenv.cc.cc.lib
+              zlib
+              glib
+            ];
+            buildInputs = with pkgs; [
+              uv
+            ];
+          in
+          ''
+            # Construct CMAKE_PREFIX_PATH by joining the store paths with semicolons
+            export CMAKE_PREFIX_PATH="${
+              pkgs.lib.makeSearchPathOutput "dev" "" deps
+            }:${pkgs.lib.makeBinPath deps}"
+
+            # Also export PKG_CONFIG_PATH just in case some libs don't have CMake configs
+            export PKG_CONFIG_PATH="${pkgs.lib.makeSearchPathOutput "dev" "lib/pkgconfig" deps}:./cmake"
+
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeLibs}"
+          '';
+
       };
     };
 }
